f48c8b3ce6e394a53e809ad4b6814cb5239c80ff
Shahin Mulani <102217599+Shahinmulani@users.noreply.github.com>
2026-09-10 00:32:52 +0530

    Time: 0 ms (100%), Space: 8.9 MB (84.8%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int64_t countCommas(auto& n) {
+        int64_t count = 0;
+        for (int64_t p = 1000; p <= n; p *= 1000)
+            count += n - p + 1;
+
+        return count;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
